98b0651da05ddd7ac9d1088771168bed2070b712
yurialeksndr <yuriatp@gmail.com>
2018-02-26 15:15:59 -0300

    Adiciona arquivo de informações a serem enviadas pelo bot.

---
+++ info.py
@@ -0,0 +1,38 @@
+#coding=UTF-8
+
+months = {
+
+	1 : "janeiro",
+	2 : "fevereiro",
+	3 : "março",
+	4 : "abril",
+	5 : "maio",
+	6 : "junho",
+	7 : "julho",
+	8 : "agosto",
+	9 : "setembro",
+	10 : "outubro",
+	11 : "novembro",
+	12 : "dezembro"
+
+}
+
+messages = {
+
+	10 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Daqui a cinco dias é a data limite para envio do calendário de {} do seu laboratório. Por favor, organizem-se para entregar na data correta. E-mails para: ana@itsbrasil.org.br e l.otavio@itsbrasil.org.br",
+
+	13 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Daqui a dois dias é a data limite para envio do calendário de {} do seu laboratório. Por favor, não atrasem o envio do documento. E-mails para: ana@itsbrasil.org.br e l.otavio@itsbrasil.org.br",
+
+	15 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Hoje é o ÚLTIMO DIA para entrega do calendário de {} do seu laboratório. Enviem até as 17h! E-mails para: ana@itsbrasil.org.br e l.otavio@itsbrasil.org.br",
+
+	19 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Amanhã é dia de enviar o relatório das impressoras de papel para a Prefeitura de São Paulo. Organizem-se para não esquecer de enviar.",
+
+	20 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Hoje é dia de enviar o relatório das impressoras de papel para a Prefeitura de São Paulo. O processo é rápido, então, por favor, não deixem fazer. E-mail para: leandroramalho@prefeitura.sp.gov.br",
+
+	25 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Hoje é dia 25 e iniciamos o período de entrega dos relatórios mensais de atividades de cada laboratório. Por favor, organizem-se para entregar até a data limite, dia 30. E-mails para: its@itsbrasil.org.br, l.otavio@itsbrasil.org.br e edilma@itsbrasil.org.br",
+
+	28 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Daqui a dois dias é a data limite para envio dos relatórios mensais de atividades de cada laboratório. Por favor, não atrasem o envio desse documento de extrema importância para a prestação de contas geral do projeto. E-mails para: its@itsbrasil.org.br, l.otavio@itsbrasil.org.br e edilma@itsbrasil.org.br",
+
+	30 : "[LEMBRETE AMIGO] Oi, equipe. Bom dia. Hoje é o último dia para entrega do relatório mensal de atividades de cada laboratório. Enviem até as 17h! E-mails para: its@itsbrasil.org.br, l.otavio@itsbrasil.org.br e edilma@itsbrasil.org.br"
+
+}
